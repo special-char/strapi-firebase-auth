0f85936bb8590675f55b8caae811f526fd5f2d19
Jil Sejpal <jilsejpal7@gmail.com>
2024-03-11 17:54:18 +0530

    commit code

--- strapi-server.js
+++ strapi-server.js
@@ -2,5 +2,5 @@
 
 module.exports =
   process.env.NODE_ENV === "local"
-    ? require("./dist/server")
-    : require("./server");
+    ? require("./dist/server").default
+    : require("./server").default;
